63ca0c2a56c0449bc52643936d767fe7b45bff42
YuriiTrofimenko <tyaa@ukr.net>
2021-11-24 19:12:41 +0200

    custom component, state, props, click handler, css style, html view

---
+++ src/components/Counter.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="counter" @click="increase">
+    {{count}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Counter',
+  props: {
+    initialCount: Number
+  },
+  data: function () {
+    return {
+      count: this.initialCount
+    }
+  },
+  methods: {
+    increase: function(){
+      this.count++
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.counter {
+  color: darkred;
+  font-size: 48px;
+  font-family: 'Times New Roman', Times, serif;
+}
+</style>
